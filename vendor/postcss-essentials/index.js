9105766e04ec43cbd61751e3520339255d96b6a3
Vasily Polovnyov <vasily@polovnyov.ru>
2015-02-28 18:03:00 +0300

    sketch postcss-essentials module

---
+++ vendor/postcss-essentials/index.js
@@ -0,0 +1,18 @@
+var postcss = require('postcss');
+var simpleVars = require('postcss-simple-vars');
+var postcssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+var nested = require('postcss-nested');
+var colorFunctions = require('postcss-color-function');
+
+module.exports = function(css, opts) {
+  var processors = [
+    postcssImport({ from: opts.from }),
+    mixins,
+    nested,
+    simpleVars,
+    colorFunctions
+  ]
+
+  return postcss(processors).process(css, opts);
+}
